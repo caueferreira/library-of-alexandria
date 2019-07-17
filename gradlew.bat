ba89451e9c2e3b41aa9061db9768350d568a2070
Caue Ferreira <eldender@gmail.com>
2019-07-16 23:31:18 -0300

    Fixing a renaming error

--- gradlew.bat
+++ gradlew.bat
@@ -8,39 +8,39 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-expansion DIRNAME=%~dp0
-if "%DIRNAME%" == "" expansion DIRNAME=.
-expansion APP_BASE_NAME=%~n0
-expansion APP_HOME=%DIRNAME%
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-expansion DEFAULT_JVM_OPTS=
+set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
-expansion JAVA_EXE=java.exe
+set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto init
 
 echo.
-echo ERROR: JAVA_HOME is not expansion and no 'java' command could be found in your PATH.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
-echo Please expansion the JAVA_HOME variable in your environment to match the
+echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
 
 goto fail
 
 :findJavaFromJavaHome
-expansion JAVA_HOME=%JAVA_HOME:"=%
-expansion JAVA_EXE=%JAVA_HOME%/bin/java.exe
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto init
 
 echo.
-echo ERROR: JAVA_HOME is expansion to an invalid directory: %JAVA_HOME%
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
-echo Please expansion the JAVA_HOME variable in your environment to match the
+echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
 
 goto fail
@@ -52,18 +52,18 @@ if not "%OS%" == "Windows_NT" goto win9xME_args
 
 :win9xME_args
 @rem Slurp the command line arguments.
-expansion CMD_LINE_ARGS=
-expansion _SKIP=2
+set CMD_LINE_ARGS=
+set _SKIP=2
 
 :win9xME_args_slurp
 if "x%~1" == "x" goto execute
 
-expansion CMD_LINE_ARGS=%*
+set CMD_LINE_ARGS=%*
 
 :execute
 @rem Setup the command line
 
-expansion CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
