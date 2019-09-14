180760b5c2f99dba9b20959bc1ae54c20365e72d
Caue Ferreira <eldender@gmail.com>
2019-09-14 01:14:17 -0300

    Fixed lint

--- cache/src/test/java/com/libraryofalexandria/cache/CacheTest.kt
+++ cache/src/test/java/com/libraryofalexandria/cache/CacheTest.kt
@@ -4,18 +4,12 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.google.gson.Gson
 import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.whenever
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.mockito.Mockito
-import org.mockito.Mock
 import org.mockito.Mockito.doNothing
-
-
-
-
 
 class CacheTest {
 
@@ -50,7 +44,7 @@ class CacheTest {
 
         fun store(list: List<TestObject>): CacheBuilder {
             whenever(sharedPrefs.edit()).thenReturn(editor)
-            whenever(editor.putString(any(),any())).thenReturn(editor)
+            whenever(editor.putString(any(), any())).thenReturn(editor)
             doNothing().`when`(editor).apply()
             cache.store(list)
 
